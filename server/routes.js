84157bf012f83d2a4232fc9653641b10e224637c
Ziqian Li <liziqian1992@gmail.com>
2022-10-11 21:35:23 -0700

    add catch-all route for rendering app from any url

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,6 @@ exports.sampleRoute = (req, res) => {
   res.sendStatus(200);
 };
 
-exports.getChat = (req, res) => {
-  res.sendStatus(200);
-};
-
 exports.catchAll = (req, res) => {
   res.sendFile(path.join(__dirname, "../client/src/index.html"));
 };
