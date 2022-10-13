535d69090e378c719b0e9120b515939372cffee1
Danting Huang <serena.huangdanting@gmail.com>
2022-10-13 02:57:21 -0400

    routes related update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import LandingPage from "./LandingPage";
 import Login from "./login/Login";
 import Signup from "./login/Signup";
 import Temporary from "./Temporary";
+import ChatsHistory from "./InstantMessaging/ChatsHistory.jsx";
 import useAuthContext from "../hooks/useAuthContext";
 
 function App() {
@@ -13,14 +14,16 @@ function App() {
   useEffect(() => {
     if (user) {
       navigate("/dashboard");
+      // navigate("/instmsgchats");
     }
   }, [user]);
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/app" element={<LandingPage />} />
       <Route path="/dashboard" element={<Temporary />} />
+      <Route path="/instmsgchats" element={<ChatsHistory />} />
     </Routes>
   );
 }
--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,9 @@
+const path = require("path");
+
 exports.sampleRoute = (req, res) => {
   res.sendStatus(200);
+};
+
+exports.catchAll = (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/src/index.html"));
 };
