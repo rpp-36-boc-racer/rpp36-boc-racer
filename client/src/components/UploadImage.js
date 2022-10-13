9c359ea59711c4170fbc4968041973c83a819eb8
Ziqian Li <liziqian1992@gmail.com>
2022-10-13 15:39:58 -0700

    add preview and remove button

--- client/src/components/UploadImage.js
+++ client/src/components/UploadImage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { uploadFile } from "react-s3";
 import useAuthContext from "../hooks/useAuthContext";
 
@@ -10,7 +10,21 @@ const config = {
 };
 
 export default function UploadImage() {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFile, setSelectedFile] = useState();
+  const [preview, setPreview] = useState();
+
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(null);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
+
+    // eslint-disable-next-line consistent-return
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [selectedFile]);
 
   const handleFileInput = (e) => {
     setSelectedFile(e.target.files[0]);
@@ -18,16 +32,37 @@ export default function UploadImage() {
 
   const handleUpload = async (file) => {
     uploadFile(file, config)
-      .then((data) => console.log(data.location))
+      .then((data) => {
+        console.log(data.location);
+        // send data.location to server
+      })
       .catch((err) => console.error(err));
   };
 
   return (
     <>
-      <div>React S3 File Upload</div>
-      <input type="file" onChange={handleFileInput} />
+      {selectedFile ? (
+        <button type="button" onClick={() => setSelectedFile(null)}>
+          Remove
+        </button>
+      ) : (
+        <button type="button">Go back to message</button>
+      )}
+      <div className="image">
+        {selectedFile ? (
+          <div>
+            <img
+              src={preview}
+              alt="preview"
+              style={{ height: 400, width: 300, objectFit: "contain" }}
+            />
+          </div>
+        ) : (
+          <input accept="image/*" type="file" onChange={handleFileInput} />
+        )}
+      </div>
       <button type="button" onClick={() => handleUpload(selectedFile)}>
-        Upload to S3
+        Upload and send
       </button>
     </>
   );
