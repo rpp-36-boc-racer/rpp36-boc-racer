ff6b6f65522a14fe6535ca38fce7360bcf6350dc
Ziqian Li <liziqian1992@gmail.com>
2022-10-14 12:55:10 -0700

    add test for send button

--- client/src/components/_tests_/sendImage.test.js
+++ client/src/components/_tests_/sendImage.test.js
@@ -1,19 +1,52 @@
 /* eslint-disable no-undef */
 import "@testing-library/jest-dom";
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
+import { AuthProvider } from "../../contexts/AuthContext";
 import SendImage from "../SendImage";
 
 describe("upload image component", () => {
   window.URL.createObjectURL = jest.fn();
   window.URL.revokeObjectURL = jest.fn();
+
+  let store = {
+    user: JSON.stringify({
+      user: {
+        username: "somebody",
+        email: "somebody@email.com",
+        profileImage: null,
+      },
+      token: "sometoken",
+    }),
+  };
+  const localStorageMock = (() => ({
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+  }))();
+
+  Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
   afterEach(() => {
     window.URL.createObjectURL.mockReset();
     window.URL.revokeObjectURL.mockReset();
   });
 
   test("image preview", async () => {
-    render(<SendImage />);
+    render(
+      <AuthProvider>
+        <SendImage />
+      </AuthProvider>
+    );
     const file = new File(["(⌐□_□)"], "somefile.png", { type: "image/png" });
     const inputEl = screen.getByTestId("select-img");
 
@@ -25,5 +58,33 @@ describe("upload image component", () => {
     expect(screen.queryByRole("img")).toBe(null);
   });
 
-  test.todo("upload image to S3 to get url");
+  test("upload and send image", async () => {
+    render(
+      <AuthProvider>
+        <SendImage />
+      </AuthProvider>
+    );
+    const file = new File(["(⌐□_□)"], "somefile.png", { type: "image/png" });
+    const inputEl = screen.getByTestId("select-img");
+    await fireEvent.change(inputEl, { target: { files: [file] } });
+    const sendButton = screen.getAllByRole("button")[1];
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            username: "somebody",
+            email: "somebody@email.com",
+            profileImage: "someimage",
+          }),
+      })
+    );
+
+    await act(() => {
+      fireEvent.click(sendButton);
+    });
+    expect(screen.queryByText("loading...")).toBe(null);
+    expect(screen.queryByText("Error")).toBe(null);
+  });
 });
