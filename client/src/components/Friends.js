c096589989b2b74ec4c356a2a469229f8c037a1d
nuotian <nuot@uci.edu>
2022-10-17 00:27:08 -0400

    tests

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -6,7 +6,7 @@ import useAuthContext from "../hooks/useAuthContext";
 
 export default function Friends() {
   const { user, dispatch } = useAuthContext();
-  const [ name, setUsername] = useState("");
+  const [name, setUsername] = useState("");
   const { error, isLoading, users, getUsers } = useGetUsers(name);
   const { addFriend } = useAddFriends(user);
 
@@ -18,29 +18,37 @@ export default function Friends() {
   const handleAdd = (e) => {
     e.preventDefault();
     const newfriend = e.target.innerHTML;
-
     addFriend({ user, newfriend });
   };
-
-  if (users.length > 0) {
+  console.log(users);
+  if (users && users.length > 0) {
     let usersEntries;
     usersEntries = users.map((person) => (
-      <li>
-        <button onClick={handleAdd}>{person}</button>
-      </li>
+      <div data-testid="user-tobe-selected-list">
+        <li>
+          <button data-testid="user-tobe-selected-button" onClick={handleAdd}>
+            {person}
+          </button>
+        </li>
+      </div>
     ));
     return (
       <WithNavBar>
         <h4>Friends Page</h4>
-        <div>Friend list of {user.username}</div>
         <input
           type="text"
-          id="myInput"
+          data-testid="myInput"
           placeholder="Search for new friends.."
           onChange={(e) => setUsername(e.target.value)}
         />
-        <button onClick={handleSubmit}>Submit</button>
-        <div>{usersEntries}</div>
+        <button
+          type="button"
+          data-testid="submit-search-btn"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+        <div data-testid="userslist">{usersEntries}</div>
       </WithNavBar>
     );
   }
@@ -53,8 +61,13 @@ export default function Friends() {
         placeholder="Search for new friends.."
         onChange={(e) => setUsername(e.target.value)}
       />
-      <button onClick={handleSubmit}>Submit</button>
-      {/* <div>Friend list of {user.username}</div> */}
+      <button
+        type="button"
+        data-testid="submit-search-btn"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
     </WithNavBar>
   );
 }
---
+++ client/src/components/_tests_/Friends.test.js
@@ -0,0 +1,118 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable no-undef */
+import React, { useEffect } from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { act } from "react-dom/test-utils";
+import { AuthProvider } from "../../contexts/AuthContext";
+import Friends from "../Friends";
+import "@testing-library/jest-dom";
+import useAuthContext from "../../hooks/useAuthContext";
+import PropTypes from "prop-types";
+
+const fakeUser = {
+  user: {
+    username: "nuotian1",
+    email: "nn123@gmail.com",
+    profileImage: "",
+    friends: ["joe", "joesph"],
+  },
+};
+
+const mockFetch = () =>
+  jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(fakeUser),
+    })
+  );
+
+// describe("Friends Page", () => {
+//   test("Add friends functionality", async () => {
+//     const user = {
+//       username: "somebody",
+//     };
+
+//     render(
+//       <BrowserRouter>
+//         <AuthContext.Provider value={{ user }}>
+//           <Friends />
+//         </AuthContext.Provider>
+//       </BrowserRouter>
+//     );
+//     expect(screen.getByText("Friend list of somebody")).toBeInTheDocument();
+//   });
+// });
+
+describe("add friends functionality", () => {
+  window.URL.createObjectURL = jest.fn();
+  window.URL.revokeObjectURL = jest.fn();
+
+  let store = {
+    user: JSON.stringify({
+      user: {
+        username: "somebody",
+        email: "somebody@email.com",
+        profileImage: null,
+        friends: ["joe", "joesph"],
+      },
+      token: "sometoken",
+    }),
+  };
+  const localStorageMock = (() => ({
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+  }))();
+
+  Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
+  afterEach(() => {
+    window.URL.createObjectURL.mockReset();
+    window.URL.revokeObjectURL.mockReset();
+  });
+
+  test("search users functionality", async () => {
+    const user = {
+      username: "somebody",
+    };
+    render(
+      <BrowserRouter>
+        <AuthProvider value={{ user }}>
+          <Friends />
+        </AuthProvider>
+      </BrowserRouter>
+    );
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            username: "somebody",
+            email: "somebody@email.com",
+            profileImage: "someimage",
+            friends: ['joe', 'joseph']
+          }),
+      })
+    );
+    const nameInput = screen.getByPlaceholderText("Search for new friends..");
+    expect(nameInput).toBeInTheDocument();
+    await act(() => fireEvent.change(nameInput, { target: { value: "joe" } }));
+    const submitButton = screen.getByTestId("submit-search-btn");
+    await act(() => fireEvent.click(submitButton));
+    const userinlistButton = screen.getByTestId("userslist");
+    expect(userinlistButton).toBeInTheDocument();
+
+    fetch.mockClear();
+  });
+});
