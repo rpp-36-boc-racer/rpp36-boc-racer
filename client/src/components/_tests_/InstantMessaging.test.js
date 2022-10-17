8b3faef85c4506413a5fc37db2b3e74be16c62d5
Ziqian Li <liziqian1992@gmail.com>
2022-10-17 15:50:28 -0700

    add test to ChatHistory

--- client/src/components/_tests_/InstantMessaging.test.js
+++ client/src/components/_tests_/InstantMessaging.test.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-undef */
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import { render, screen, act, fireEvent } from "@testing-library/react";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 // eslint-disable-next-line import/no-unresolved
 import ChatsHistory from "../InstantMessaging/ChatsHistory.jsx";
 import FriendMessage from "../InstantMessaging/FriendMessage.jsx";
 import OwnerMessage from "../InstantMessaging/OwnerMessage.jsx";
 import "@testing-library/jest-dom";
+
+// Note: In order to mock properly, Jest needs jest.mock('moduleName') to be in the same scope as the require/import statement.
+jest.mock("axios");
 
 describe("Instant message page", () => {
   let store = {
@@ -34,103 +38,102 @@ describe("Instant message page", () => {
       delete store[key];
     },
   }))();
-
   Object.defineProperty(window, "localStorage", { value: localStorageMock });
-  test("Renders the chat history page", async () => {
-    const scrollRef = { current: { scrollIntoView: () => {} } };
-    const user = {
-      _id: "member1",
-      username: "somebody",
-      profileImage: "img1.jpg",
-    };
-    const friend = {
-      _id: "member2",
-      username: "someFriend",
-      profileImage: "img2.jpg",
-    };
-    const curConversation = {
-      _id: "convo1",
-      members: ["member1", "member2"],
-    };
-    const messages = [
-      {
-        _id: "msg1",
-        conversationID: "convo1",
-        text: "hello from member1",
-        senderID: "member1",
-      },
-      {
-        _id: "msg2",
-        conversationID: "convo1",
-        text: "hello from member2",
-        senderID: "member2",
-      },
-      {
-        _id: "msg3",
-        conversationID: "convo1",
-        photoUrl: "photoMessage1.jpg",
-        senderID: "member2",
-      },
-    ];
 
-    render(
-      <BrowserRouter>
-        <AuthContext.Provider value={{ user }}>
-          <ChatsHistory
-            user={user}
-            friend={friend}
-            curConversation={curConversation}
-            messages={messages}
-            scrollRef={scrollRef}
-          />
-        </AuthContext.Provider>
-      </BrowserRouter>
-    );
-    expect(screen.getByText("DM with member2 as member1")).toBeInTheDocument();
-    expect(screen.getByText("hello from member1")).toBeInTheDocument();
-    expect(screen.getByText("hello from member2")).toBeInTheDocument();
-  });
-  test("Renders test and photo messages sent by user", async () => {
-    const user = {
-      _id: "member1",
-      username: "somebody",
-      profileImage: "img1.jpg",
+  // mock data
+  const user = {
+    _id: "member1",
+    username: "somebody",
+    profileImage: "img1.jpg",
+  };
+  const friend = {
+    _id: "member2",
+    username: "someFriend",
+    profileImage: "img2.jpg",
+  };
+  const curConversation = {
+    _id: "convo1",
+    members: ["member1", "member2"],
+  };
+
+  const messages = [
+    {
+      _id: "msg1",
+      conversationID: "convo1",
+      text: "hello from member1",
+      senderID: "member1",
+    },
+    {
+      _id: "msg2",
+      conversationID: "convo1",
+      text: "hello from member2",
+      senderID: "member2",
+    },
+    {
+      _id: "msg3",
+      conversationID: "convo1",
+      photoUrl: "photoMessage1.jpg",
+      senderID: "member2",
+    },
+    {
+      _id: "msg4",
+      conversationID: "convo1",
+      photoUrl: "photoMessage2.jpg",
+      senderID: "member1",
+    },
+  ];
+
+  test("Renders the chat history page", async () => {
+    // jest does not implement useRef by default. Needs to add scrollIntoView fn into the jest-dom window
+    window.HTMLElement.prototype.scrollIntoView = () => {};
+
+    const mockUseLocationValue = {
+      pathname: "/messaging",
+      state: {
+        conversationId: curConversation._id,
+        friendId: friend._id,
+        profileImage: friend.profileImage,
+        username: friend.username,
+      },
     };
-    const friend = {
-      _id: "member2",
-      username: "someFriend",
-      profileImage: "img2.jpg",
-    };
-    const curConversation = {
-      _id: "convo1",
-      members: ["member1", "member2"],
-    };
-    const messages = [
+
+    const mockMessageData = [
       {
-        _id: "msg1",
-        conversationID: "convo1",
         text: "hello from member1",
         senderID: "member1",
       },
       {
-        _id: "msg2",
-        conversationID: "convo1",
         text: "hello from member2",
         senderID: "member2",
       },
       {
-        _id: "msg3",
-        conversationID: "convo1",
         photoUrl: "photoMessage1.jpg",
         senderID: "member2",
       },
       {
-        _id: "msg4",
-        conversationID: "convo1",
         photoUrl: "photoMessage2.jpg",
         senderID: "member1",
       },
     ];
+
+    axios.get.mockResolvedValue({ data: mockMessageData });
+    await act(() => {
+      render(
+        <AuthContext.Provider value={{ user }}>
+          <MemoryRouter initialEntries={[mockUseLocationValue]}>
+            <ChatsHistory />
+          </MemoryRouter>
+        </AuthContext.Provider>
+      );
+    });
+    expect(
+      screen.getByText("DM with someFriend as somebody")
+    ).toBeInTheDocument();
+    expect(screen.getByText("hello from member1")).toBeInTheDocument();
+    expect(screen.getByText("hello from member2")).toBeInTheDocument();
+  });
+
+  test("Renders test and photo messages sent by user", async () => {
 
     const { getByAltText } = await render(
       <BrowserRouter>
@@ -153,41 +156,8 @@ describe("Instant message page", () => {
     expect(screen.getByText("hello from member1")).toBeInTheDocument();
     expect(image.src).toContain("photoMessage2.jpg");
   });
+
   test("Renders text and photo messages sent by friend", async () => {
-    const user = {
-      _id: "member1",
-      username: "somebody",
-      profileImage: "img1.jpg",
-    };
-    const friend = {
-      _id: "member2",
-      username: "someFriend",
-      profileImage: "img2.jpg",
-    };
-    const curConversation = {
-      _id: "convo1",
-      members: ["member1", "member2"],
-    };
-    const messages = [
-      {
-        _id: "msg1",
-        conversationID: "convo1",
-        text: "hello from member1",
-        senderID: "member1",
-      },
-      {
-        _id: "msg2",
-        conversationID: "convo1",
-        text: "hello from member2",
-        senderID: "member2",
-      },
-      {
-        _id: "msg3",
-        conversationID: "convo1",
-        photoUrl: "photoMessage1.jpg",
-        senderID: "member2",
-      },
-    ];
 
     const { getByAltText } = await render(
       <BrowserRouter>
