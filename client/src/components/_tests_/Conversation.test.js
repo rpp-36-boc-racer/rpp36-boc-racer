f80414d1a15b230a52837e137d89da90215cdfec
Ziqian Li <liziqian1992@gmail.com>
2022-10-17 20:40:24 -0700

    fix bugs and resolve tests

--- client/src/components/_tests_/Conversation.test.js
+++ client/src/components/_tests_/Conversation.test.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { act } from 'react-dom/test-utils';
-import Conversation from '../MessageList/Conversation';
-import {render, screen} from '@testing-library/react';
-import '@testing-library/jest-dom';
+/* eslint-disable no-undef */
+import "@testing-library/jest-dom";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import Conversation from "../MessageList/Conversation";
 
-describe('Conversation', () => {
+describe("Conversation", () => {
   let temporarySandBox;
   beforeEach(() => {
-    temporarySandBox = document.createElement('div');
+    temporarySandBox = document.createElement("div");
     document.body.appendChild(temporarySandBox);
   });
   afterEach(() => {
@@ -17,17 +16,18 @@ describe('Conversation', () => {
     temporarySandBox = null;
   });
 
-  describe('Conversation Component', ()=>{
-    const convo = {
-      "senderId": "001",
-      "time": "1665725713",
-      "text": "this is a sample message"
-    }
+  const convo = {
+    senderId: "001",
+    time: "1665725713",
+    text: "this is a sample message",
+  };
 
-    it('should have a delete button', async () => {
-      render(<Conversation convo={convo} />);
-      expect(screen.getByTestId('delete')).toBeInTheDocument();
-    });
+  it("should have a delete button", async () => {
+    render(
+      <BrowserRouter>
+        <Conversation convo={convo} />
+      </BrowserRouter>
+    );
+    expect(screen.getByTestId("delete")).toBeInTheDocument();
   });
 });
-
