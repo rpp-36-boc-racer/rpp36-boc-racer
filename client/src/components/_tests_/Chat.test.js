08dbff39ac2862d56d815d022cd70c1f5a69c14a
Ziqian Li <liziqian1992@gmail.com>
2022-10-17 22:50:56 -0700

    fix some test

--- client/src/components/_tests_/Chat.test.js
+++ client/src/components/_tests_/Chat.test.js
@@ -2,32 +2,46 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
-import Chat from "../Chat";
+import Chat from "../MessageList/Chat";
 import "@testing-library/jest-dom";
 
-describe("Friends list", () => {
-  test("Renders friend list", async () => {
+describe("Message List", () => {
+  test("Renders message list", async () => {
     const user = {
-      username: "somebody",
+      user: "me",
     };
+    const data = [
+      {
+        conversationId: "convo",
+        friendId: "123",
+        profileImage: "photo.jpeg",
+        text: "hello test",
+        username: "somebody",
+        time: "2022-10-18T05:14:24.077Z",
+      },
+    ];
 
     global.fetch = jest.fn(() =>
       Promise.resolve({
         ok: true,
-        json: () => Promise.resolve(user),
+        json: () => Promise.resolve(data),
       })
     );
 
-    render(
-      <BrowserRouter>
-        <AuthContext.Provider value={{ user }}>
-          <Chat />
-        </AuthContext.Provider>
-      </BrowserRouter>
-    );
-    expect(screen.getByText("Message list of somebody")).toBeInTheDocument();
+    await act(() => {
+      render(
+        <BrowserRouter>
+          <AuthContext.Provider value={{ user }}>
+            <Chat />
+          </AuthContext.Provider>
+        </BrowserRouter>
+      );
+    });
+    expect(screen.getByText("somebody")).toBeInTheDocument();
+    expect(screen.getByText("hello test")).toBeInTheDocument();
   });
 
   it.todo("more test?");
