6d7a7d102ee8d2621bee6cc261dffc44d7ca959d
nuotian <nuot@uci.edu>
2022-10-20 17:30:20 -0400

    move addfriends component out of the friend page

--- client/src/components/AddFriends.js
+++ client/src/components/AddFriends.js
@@ -1,13 +1,189 @@
-import React from "react";
-import useAuthContext from "../hooks/useAuthContext";
+import React, { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import InputAdornment from "@mui/material/InputAdornment";
+import Icon from "@mui/material/Icon";
+import { IconButton } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import useGetUsers from "../hooks/useGetUsers";
 import WithNavBar from "./withNavBar";
+import useAddFriends from "../hooks/useAddFriends";
+import useAuthContext from "../hooks/useAuthContext";
 
-export default function Chat() {
-  const { user } = useAuthContext();
+export default function Friends() {
+  const { user, dispatch } = useAuthContext();
+  const [name, setUsername] = useState("");
+  const [friendList, setFriendList] = useState(null);
+  const { error, isLoading, users, getUsers } = useGetUsers(name);
+  const { addFriend } = useAddFriends(user);
 
+  useEffect(() => {
+    if (users) {
+      getUsers({ name });
+    }
+  }, [users]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getUsers({ name });
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const newfriend = e.target.id;
+    addFriend({ user, newfriend });
+    getFriends();
+  };
+
+  const newuserslist = users.filter(
+    (person) => person.username !== user.username
+  );
+
+  if (users && users.length > 0) {
+    let usersEntries;
+    usersEntries = newuserslist.map((person) => {
+      if (person.friends && person.friends.includes(user.username)) {
+        return (
+          <div data-testid="user-tobe-selected-list">
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="profilepic" src={person.profilepic} />
+              </ListItemAvatar>
+              <ListItemText primary={person.username} />
+              <button
+                type="button"
+                data-testid="user-tobe-selected-button"
+                id={person.username}
+                onClick={handleAdd}
+              >
+                chat
+              </button>
+            </ListItem>
+          </div>
+        );
+      }
+      return (
+        <div data-testid="user-tobe-selected-list">
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="profilepic" src={person.profilepic} />
+            </ListItemAvatar>
+            <ListItemText primary={person.username} />
+
+            <button
+              type="button"
+              data-testid="user-tobe-selected-button"
+              id={person.username}
+              onClick={handleAdd}
+            >
+              add
+            </button>
+          </ListItem>
+        </div>
+      );
+    });
+
+    return (
+      <>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "30ch" },
+          }}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            type="text"
+            data-testid="myInput"
+            label="USERNAME"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+            variant="outlined"
+            placeholder="Search for new friends.."
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <div style={{ width: "20%", display: "flex" }}>
+            <IconButton
+              aria-label="add friend"
+              size="large"
+              data-testid="submit-search-btn"
+              sx={{ display: "flex" }}
+              onClick={handleSubmit}
+            >
+              <AddCircleIcon fontSize="large" />
+            </IconButton>
+          </div>
+        </Box>
+        <div data-testid="userslist">{usersEntries}</div>
+        <div>Friend list</div>
+        {friendList && (
+          <ul>
+            {friendList.map((friend) => (
+              <div key={friend}>
+                <li>{friend}</li>
+                <Button onClick={() => chat(friend)}>Chat</Button>
+              </div>
+            ))}
+          </ul>
+        )}
+      </>
+    );
+  }
   return (
-    <WithNavBar>
-      <h4>Add Friend</h4>
-    </WithNavBar>
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "30ch" },
+      }}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        type="text"
+        data-testid="myInput"
+        label="USERNAME"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        variant="outlined"
+        placeholder="Search for new friends.."
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <div style={{ width: "20%", display: "flex" }}>
+        <IconButton
+          aria-label="add friend"
+          size="large"
+          data-testid="submit-search-btn"
+          sx={{ display: "flex" }}
+          onClick={handleSubmit}
+        >
+          <AddCircleIcon fontSize="large" />
+        </IconButton>
+      </div>
+    </Box>
   );
 }
--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -17,13 +17,10 @@ import useGetUsers from "../hooks/useGetUsers";
 import WithNavBar from "./withNavBar";
 import useAddFriends from "../hooks/useAddFriends";
 import useAuthContext from "../hooks/useAuthContext";
+import AddFriends from "./AddFriends";
 
 export default function Friends() {
   const { user, dispatch } = useAuthContext();
-  const [name, setUsername] = useState("");
-  const [friendList, setFriendList] = useState(null);
-  const { error, isLoading, users, getUsers } = useGetUsers(name);
-  const { addFriend } = useAddFriends(user);
   const navigate = useNavigate();
 
   const getFriends = async () => {
@@ -49,169 +46,10 @@ export default function Friends() {
     navigate("/messaging", { state: { friend } });
   };
 
-  useEffect(() => {
-    if (!users) {
-      getUsers({ name });
-    }
-  }, [users]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    getUsers({ name });
-  };
-
-  const handleAdd = (e) => {
-    e.preventDefault();
-    const newfriend = e.target.id;
-    addFriend({ user, newfriend });
-    getFriends();
-  };
-
-  const newuserslist = users.filter(
-    (person) => person.username !== user.username
-  );
-
-  if (users && users.length > 0) {
-    let usersEntries;
-    usersEntries = newuserslist.map((person) => {
-      if (person.friends && person.friends.includes(user.username)) {
-        return (
-          <div data-testid="user-tobe-selected-list">
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar alt="profilepic" src={person.profilepic} />
-              </ListItemAvatar>
-              <ListItemText primary={person.username} />
-              <button
-                type="button"
-                data-testid="user-tobe-selected-button"
-                id={person.username}
-                onClick={handleAdd}
-              >
-                chat
-              </button>
-            </ListItem>
-          </div>
-        );
-      }
-      return (
-        <div data-testid="user-tobe-selected-list">
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <Avatar alt="profilepic" src={person.profilepic} />
-            </ListItemAvatar>
-            <ListItemText primary={person.username} />
-
-            <button
-              type="button"
-              data-testid="user-tobe-selected-button"
-              id={person.username}
-              onClick={handleAdd}
-            >
-              add
-            </button>
-          </ListItem>
-        </div>
-      );
-    });
-
-    return (
-      <WithNavBar>
-        <h4>Friends Page</h4>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "30ch" },
-          }}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            type="text"
-            data-testid="myInput"
-            label="USERNAME"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-            placeholder="Search for new friends.."
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <div style={{ width: "20%", display: "flex" }}>
-            <IconButton
-              aria-label="add friend"
-              size="large"
-              data-testid="submit-search-btn"
-              sx={{ display: "flex" }}
-              onClick={handleSubmit}
-            >
-              <AddCircleIcon fontSize="large" />
-            </IconButton>
-          </div>
-        </Box>
-        <div data-testid="userslist">{usersEntries}</div>
-        <div>Friend list</div>
-        {friendList && (
-          <ul>
-            {friendList.map((friend) => (
-              <div key={friend}>
-                <li>{friend}</li>
-                <Button onClick={() => chat(friend)}>Chat</Button>
-              </div>
-            ))}
-          </ul>
-        )}
-      </WithNavBar>
-    );
-  }
   return (
     <WithNavBar>
       <h4>Friends Page</h4>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "30ch" },
-        }}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          type="text"
-          data-testid="myInput"
-          label="USERNAME"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            ),
-          }}
-          variant="outlined"
-          placeholder="Search for new friends.."
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <div style={{ width: "20%", display: "flex" }}>
-          <IconButton
-            aria-label="add friend"
-            size="large"
-            data-testid="submit-search-btn"
-            sx={{ display: "flex" }}
-            onClick={handleSubmit}
-          >
-            <AddCircleIcon fontSize="large" />
-          </IconButton>
-        </div>
-      </Box>
+      <AddFriends />
       <div>Friend list</div>
       {friendList && (
         <ul>
