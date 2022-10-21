088cd37df2496e52d3717673bf11a93bbb85af70
Ziqian Li <liziqian1992@gmail.com>
2022-10-20 22:49:30 -0700

    comment out unused code

--- server/db/index.js
+++ server/db/index.js
@@ -79,83 +79,83 @@ exports.setProfileImage = async (_id, url) => {
   return user;
 };
 
-exports.saveMessage = async (messageData) => {
-  const message = await db.Message.create(messageData);
-  return message;
-};
+// exports.saveMessage = async (messageData) => {
+//   const message = await db.Message.create(messageData);
+//   return message;
+// };
 
-exports.getConversations = async (_id) => {
-  const obj = {};
-  const results = await db.Message.find({
-    $or: [{ userId: _id }, { friendId: _id }],
-  }).sort({ createdAt: -1 });
-  results.forEach((result) => {
-    if (!obj[result.friendId]) {
-      obj[result.friendId] = result;
-    }
-  });
-  const keys = Object.keys(obj);
-  const messages = [];
-  keys.forEach((key) => {
-    messages.push(obj[key]);
-  });
-  const conversations = await Promise.all(
-    messages.map((message) =>
-      db.User.findOne({ _id: message.friendId }).then((user) => ({
-        message: message.message,
-        createdAt: message.createdAt,
-        friend: {
-          username: user.username,
-          _id: user._id,
-          profileImage: user.profileImage,
-        },
-      }))
-    )
-  );
-  return conversations;
-};
+// exports.getConversations = async (_id) => {
+//   const obj = {};
+//   const results = await db.Message.find({
+//     $or: [{ userId: _id }, { friendId: _id }],
+//   }).sort({ createdAt: -1 });
+//   results.forEach((result) => {
+//     if (!obj[result.friendId]) {
+//       obj[result.friendId] = result;
+//     }
+//   });
+//   const keys = Object.keys(obj);
+//   const messages = [];
+//   keys.forEach((key) => {
+//     messages.push(obj[key]);
+//   });
+//   const conversations = await Promise.all(
+//     messages.map((message) =>
+//       db.User.findOne({ _id: message.friendId }).then((user) => ({
+//         message: message.message,
+//         createdAt: message.createdAt,
+//         friend: {
+//           username: user.username,
+//           _id: user._id,
+//           profileImage: user.profileImage,
+//         },
+//       }))
+//     )
+//   );
+//   return conversations;
+// };
 
-exports.getMessages = async (userId, friendId) => {
-  const messages1 = await db.Message.find({ userId, friendId });
-  const messages2 = await db.Message.find({
-    userId: friendId,
-    friendId: userId,
-  });
-  const messages = [];
-  messages1.forEach((message) => {
-    messages.push(message);
-  });
-  messages2.forEach((message) => {
-    messages.push(message);
-  });
-  // console.log("Messages from me", messages2);
-  messages.sort((a, b) => {
-    if (a.createdAt < b.createdAt) {
-      return -1;
-    }
-    return 1;
-  });
+// exports.getMessages = async (userId, friendId) => {
+//   const messages1 = await db.Message.find({ userId, friendId });
+//   const messages2 = await db.Message.find({
+//     userId: friendId,
+//     friendId: userId,
+//   });
+//   const messages = [];
+//   messages1.forEach((message) => {
+//     messages.push(message);
+//   });
+//   messages2.forEach((message) => {
+//     messages.push(message);
+//   });
+//   // console.log("Messages from me", messages2);
+//   messages.sort((a, b) => {
+//     if (a.createdAt < b.createdAt) {
+//       return -1;
+//     }
+//     return 1;
+//   });
 
-  const friend = await db.User.findOne({ _id: friendId });
-  return messages.map((message) => {
-    if (message.friendId !== userId) {
-      return {
-        message: message.message,
-        createdAt: message.createdAt,
-        username: friend.username,
-        profileImage: friend.profileImage,
-        friendId: friend._id,
-        _id: message._id,
-      };
-    }
-    return {
-      message: message.message,
-      createdAt: message.createdAt,
-      _id: message._id,
-      username: "Me",
-    };
-  });
-};
+//   const friend = await db.User.findOne({ _id: friendId });
+//   return messages.map((message) => {
+//     if (message.friendId !== userId) {
+//       return {
+//         message: message.message,
+//         createdAt: message.createdAt,
+//         username: friend.username,
+//         profileImage: friend.profileImage,
+//         friendId: friend._id,
+//         _id: message._id,
+//       };
+//     }
+//     return {
+//       message: message.message,
+//       createdAt: message.createdAt,
+//       _id: message._id,
+//       username: "Me",
+//     };
+//   });
+// };
 
 exports.getUserInfo = async (_id) => {
   const userInfo = await db.User.findOne({ _id });
